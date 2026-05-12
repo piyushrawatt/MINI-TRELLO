869a71f85f6d277faffcaa7508f145d61462d2b4
piyushrawatt <piyushrawatt018@gmail.com>
2026-05-12 17:42:18 +0530

    updated project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes,Route} from "react-router-dom"
-import Trellomain from './trellomain'
+import Trellomain from './Trellomain'
 import Login from './Login'
 import Signup from './Signup'
 function App() {
